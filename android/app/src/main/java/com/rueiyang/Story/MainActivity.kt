fea390317819301ebf9c59d1e7175aba0e46c399
gjxxxdev <jgxxxdev@gmail.com>
2025-07-08 10:27:23 +0800

    新增xcode project 生成版本工作流

--- android/app/src/main/java/com/rueiyang/Story/MainActivity.kt
+++ android/app/src/main/java/com/rueiyang/Story/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rueiyang.Story
+package com.rueiyang.story
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/rueiyang/Story/MainApplication.kt
+++ android/app/src/main/java/com/rueiyang/Story/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rueiyang.Story
+package com.rueiyang.story
 
 import android.app.Application
 import android.content.res.Configuration
